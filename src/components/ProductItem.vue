5e86766102cc9ccf1c2d39c9e37960284d5df10c
Juanma Cano <juan.cano.external@atos.net>
2020-08-02 11:00:21 +0200

    vuex persistent

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -6,8 +6,8 @@
     style="max-width: 20rem; padding: 1rem;"
     class="shadow-sm"
   >
-    <b-card-text class="font-weight-bold">{{ product.name }} ...</b-card-text>
-    <b-card-text>{{ product.description.substr(0, 80) }} ...</b-card-text>
+    <b-card-text class="font-weight-bold">{{ product.name }}</b-card-text>
+
     <template v-slot:footer>
       <b-button block variant="warning" @click="$emit('addToCart', product)">Añadir al carrito</b-button>
     </template>
